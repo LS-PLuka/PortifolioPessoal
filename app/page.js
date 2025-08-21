5ebee435aec84f677e8b640f63530811a6e66a93
Pedro Luka <pedrolukagt@gmail.com>
2025-08-20 21:42:48 -0300

    componente header

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      
+      <Header />
     </div>
   );
 }
---
+++ components/Header/index.js
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <a href="#home"><h1>Pedro Luka</h1></a>
+      <nav>
+        <ul>
+          <li><a href="#projetos">Projetos</a></li>
+          <li><a href="#sobre">Sobre</a></li>
+          <li><a href="#cursos">Cursos</a></li>
+          <li><a href="#formacoes">Formações</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
