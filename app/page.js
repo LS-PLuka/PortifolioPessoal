0c2171256f44364391ba2fa0f1a65be354a70f44
Pedro Luka <pedrolukagt@gmail.com>
2025-08-21 20:53:17 -0300

    finalizando versao 1.0 sessao projetos

--- app/page.js
+++ app/page.js
@@ -54,14 +54,30 @@ export default function Home() {
 
       <hr className={styles.hr}></hr>
 
-      <div className={styles.projects}>
+      <div id="projetos" className={styles.projects}>
         <div className={styles.title_desc}>
           <h2>Projetos</h2>
           <p>Conheça meus projetos em destaque.</p>
         </div>
 
-        <div className={styles.card_projects}>
+        <div className={styles.cards_container}>
+          <a href="https://github.com/LS-PLuka/MoveON.git" className={styles.card_projects}>
+            <img src="./moveon.png" alt="MoveON" />
+            <h1>MoveON</h1>
+            <p>Projeto Fullstack (HTML, CSS e JS).</p>
+          </a>
 
+          <a href="https://github.com/LS-PLuka/OutroProjeto.git" className={styles.card_projects}>
+            <img src="./outro.png" alt="Outro" />
+            <h1>Outro Projeto</h1>
+            <p>Descrição rápida do projeto.</p>
+          </a>
+
+          <a href="https://github.com/LS-PLuka/Terceiro.git" className={styles.card_projects}>
+            <img src="./terceiro.png" alt="Terceiro" />
+            <h1>Terceiro</h1>
+            <p>Descrição rápida do projeto.</p>
+          </a>
         </div>
       </div>
     </div>
